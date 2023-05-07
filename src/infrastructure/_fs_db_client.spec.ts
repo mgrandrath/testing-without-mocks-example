3b8fe3462b1be13eb386831728e3ef99ea0ae3e3
Martin Grandrath <martin.grandrath@codecentric.de>
2023-05-07 08:57:42 +0200

    let null FsDbClient throw configurable errors and improve test coverage

--- src/infrastructure/_fs_db_client.spec.ts
+++ src/infrastructure/_fs_db_client.spec.ts
@@ -1,8 +1,26 @@
 import { afterEach, beforeEach, describe, expect, it } from "vitest";
 import { isNone, some } from "fp-ts/lib/Option";
-import { FsDbClient } from "./fs_db_client";
+import { FsDbClient, assertValidId } from "./fs_db_client";
 import { recordEvents } from "../spec-helpers/record-events";
 import { TmpFile, createTmpDbFile } from "../spec-helpers/tmp-file";
+
+describe("assertValidId", () => {
+  it("should throw when item id is not a string", () => {
+    expect(() => assertValidId(123)).toThrow(
+      "Expected id to be a string, but got 'number'."
+    );
+  });
+
+  it("should throw when item id is blank", async () => {
+    expect(() => assertValidId("")).toThrow("id cannot be blank");
+  });
+
+  it("should throw when item id contains reserved character '/'", () => {
+    expect(() => assertValidId("this/is/invalid")).toThrow(
+      "The '/' character is not allowed in item ids. Invalid value was 'this/is/invalid'."
+    );
+  });
+});
 
 describe("FsDbClient", () => {
   describe("real instance", () => {
@@ -67,19 +85,6 @@ describe("FsDbClient", () => {
         some({
           data: "this is my data",
         })
-      );
-    });
-
-    it("should throw when item id contains reserved character '/'", async () => {
-      type Item = { someData: string };
-      const fsDbClient = FsDbClient.create<Item>({
-        dbFile: tmpFile.path,
-      });
-
-      await expect(() =>
-        fsDbClient.putItem("this/is/invalid", { someData: "irrelevant" })
-      ).rejects.toThrow(
-        "The '/' character is not allowed in item ids. Invalid value was 'this/is/invalid'."
       );
     });
 
@@ -163,69 +168,135 @@ describe("FsDbClient", () => {
       await fsDbClient.deleteItem("item-111");
     });
 
-    it("should respond with configured items", async () => {
-      type Item = { id: string; data: number };
-      const fsDbClient = FsDbClient.createNull<Item>({
-        items: {
-          "item-111": {
+    describe("getItem", () => {
+      it("should respond with a configured item", async () => {
+        type Item = { id: string; data: number };
+        const fsDbClient = FsDbClient.createNull<Item>({
+          items: {
+            "item-111": {
+              id: "item-111",
+              data: 111,
+            },
+            "item-222": {
+              id: "item-222",
+              data: 222,
+            },
+          },
+        });
+
+        expect(await fsDbClient.getItem("item-111")).toEqual(
+          some({
+            id: "item-111",
+            data: 111,
+          })
+        );
+      });
+
+      it("should throw a configurable error", async () => {
+        const fsDbClient = FsDbClient.createNull({
+          error: new Error("some error"),
+        });
+
+        await expect(fsDbClient.getItem("irrelevant-id")).rejects.toThrow(
+          "some error"
+        );
+      });
+    });
+
+    describe("listItems", () => {
+      it("should respond with configured items", async () => {
+        type Item = { id: string; data: number };
+        const fsDbClient = FsDbClient.createNull<Item>({
+          items: {
+            "item-111": {
+              id: "item-111",
+              data: 111,
+            },
+            "item-222": {
+              id: "item-222",
+              data: 222,
+            },
+          },
+        });
+
+        expect(await fsDbClient.listItems()).toEqual([
+          {
             id: "item-111",
             data: 111,
           },
-          "item-222": {
+          {
             id: "item-222",
             data: 222,
           },
-        },
+        ]);
       });
 
-      expect(await fsDbClient.listItems()).toEqual([
-        {
-          id: "item-111",
-          data: 111,
-        },
-        {
-          id: "item-222",
-          data: 222,
-        },
-      ]);
-      expect(await fsDbClient.getItem("item-111")).toEqual(
-        some({
-          id: "item-111",
-          data: 111,
-        })
-      );
+      it("should throw a configurable error", async () => {
+        const fsDbClient = FsDbClient.createNull({
+          error: new Error("some error"),
+        });
+
+        await expect(fsDbClient.listItems()).rejects.toThrow("some error");
+      });
     });
 
-    it("should emit an event when an item has been stored", async () => {
-      type Item = { data: string };
-      const fsDbClient = FsDbClient.createNull<Item>();
-      const itemStoredEvents = recordEvents(fsDbClient, FsDbClient.ITEM_STORED);
+    describe("putItem", () => {
+      it("should emit an event when an item has been stored", async () => {
+        type Item = { data: string };
+        const fsDbClient = FsDbClient.createNull<Item>();
+        const itemStoredEvents = recordEvents(
+          fsDbClient,
+          FsDbClient.ITEM_STORED
+        );
 
-      await fsDbClient.putItem("item-111", {
-        data: "some data",
-      });
+        await fsDbClient.putItem("item-111", {
+          data: "some data",
+        });
 
-      expect(itemStoredEvents).toEqual([
-        {
-          id: "item-111",
-          item: {
-            data: "some data",
+        expect(itemStoredEvents).toEqual([
+          {
+            id: "item-111",
+            item: {
+              data: "some data",
+            },
           },
-        },
-      ]);
+        ]);
+      });
+
+      it("should throw a configurable error", async () => {
+        const fsDbClient = FsDbClient.createNull({
+          error: new Error("some error"),
+        });
+
+        await expect(fsDbClient.putItem("irrelevant-id", {})).rejects.toThrow(
+          "some error"
+        );
+      });
     });
 
-    it("should emit an event when an item has been deleted", async () => {
-      type Item = { data: string };
-      const fsDbClient = FsDbClient.createNull<Item>();
-      const itemDeletedEvents = recordEvents(
-        fsDbClient,
-        FsDbClient.ITEM_DELETED
-      );
+    describe("deleteItem", () => {
+      it("should emit an event when an item has been deleted", async () => {
+        type Item = { data: string };
+        const fsDbClient = FsDbClient.createNull<Item>();
+        const itemDeletedEvents = recordEvents(
+          fsDbClient,
+          FsDbClient.ITEM_DELETED
+        );
 
-      await fsDbClient.deleteItem("item-111");
+        await fsDbClient.deleteItem("item-111");
 
-      expect(itemDeletedEvents).toEqual([{ id: "item-111" }]);
+        expect(itemDeletedEvents).toEqual([{ id: "item-111" }]);
+      });
+
+      it("should throw a configurable error", async () => {
+        const fsDbClient = FsDbClient.createNull({
+          error: new Error("some error"),
+        });
+
+        await expect(fsDbClient.deleteItem("irrelevant-id")).rejects.toThrow(
+          "some error"
+        );
+      });
     });
   });
 });
--- src/infrastructure/fs_db_client.ts
+++ src/infrastructure/fs_db_client.ts
@@ -9,7 +9,7 @@ import {
 import { Option, none, some } from "fp-ts/lib/Option";
 
 type FsDbClientOptions = { dbFile: string };
-type NullFsDbClientOptions<T> = { items?: Record<string, T> };
+type NullFsDbClientOptions<T> = { items?: Record<string, T>; error?: Error };
 type Item = Record<string, unknown>;
 type Id = string & { __brand: "Id" };
 type JsonDbInterface = Pick<JsonDB, "getData" | "push" | "delete">;
@@ -17,6 +17,22 @@ type JsonDbInterface = Pick<JsonDB, "getData" | "push" | "delete">;
 // Error ids used as magic numbers inside node-json-db
 const JSON_PARSE_ERROR = 1;
 const DATA_PATH_NOT_FOUND = 5;
+
+export function assertValidId(id: unknown): asserts id is Id {
+  if (typeof id !== "string") {
+    throw new Error(`Expected id to be a string, but got '${typeof id}'.`);
+  }
+
+  if (id === "") {
+    throw new Error("id cannot be blank");
+  }
+
+  if (id.includes("/")) {
+    throw new Error(
+      `The '/' character is not allowed in item ids. Invalid value was '${id}'.`
+    );
+  }
+}
 
 export class FsDbClient<T extends Item> extends EventEmitter {
   static create<T extends Item>(options: FsDbClientOptions) {
@@ -55,7 +71,7 @@ export class FsDbClient<T extends Item> extends EventEmitter {
   }
 
   async getItem(id: string): Promise<Option<T>> {
-    this.#assertValidId(id);
+    assertValidId(id);
 
     try {
       return some(await this.#jsonDb.getData(this.#itemPath(id)));
@@ -74,7 +90,7 @@ export class FsDbClient<T extends Item> extends EventEmitter {
   }
 
   async putItem(id: string, item: T): Promise<void> {
-    this.#assertValidId(id);
+    assertValidId(id);
 
     try {
       await this.#jsonDb.push(this.#itemPath(id), item, /* overwrite */ true);
@@ -91,7 +107,7 @@ export class FsDbClient<T extends Item> extends EventEmitter {
   }
 
   async deleteItem(id: string): Promise<void> {
-    this.#assertValidId(id);
+    assertValidId(id);
 
     try {
       await this.#jsonDb.delete(this.#itemPath(id));
@@ -109,18 +125,6 @@ export class FsDbClient<T extends Item> extends EventEmitter {
 
   #itemPath(id: Id) {
     return `/${id}`;
-  }
-
-  #assertValidId(id: unknown): asserts id is Id {
-    if (typeof id !== "string") {
-      throw new Error(`Expected id to be a string, but got '${typeof id}'.`);
-    }
-
-    if (id.includes("/")) {
-      throw new Error(
-        `The '/' character is not allowed in item ids. Invalid value was '${id}'.`
-      );
-    }
   }
 
   #isDbUninitialized(error: unknown) {
@@ -141,28 +145,34 @@ const createJsonDbStub = <T>(
 ): JsonDbInterface => {
   return {
     async getData(dataPath) {
+      if (options?.error) {
+        throw options.error;
+      }
+
       if (dataPath === "/") {
         return options?.items ?? {};
       } else {
-        const [_, id] = /^\/(.*)/.exec(dataPath) ?? [];
-        if (
-          !id ||
-          !options?.items ||
-          !Object.prototype.hasOwnProperty.call(options?.items, id)
-        ) {
+        const id = dataPath.substring(1); // remove leading '/'
+        const item = options?.items?.[id];
+
+        if (!item) {
           throw new DataError("Can't find dataPath", DATA_PATH_NOT_FOUND);
         }
 
-        return options?.items[id];
+        return item;
       }
     },
 
     async push(_dataPath, _data, _override) {
-      // noop
+      if (options?.error) {
+        throw options.error;
+      }
     },
 
     async delete(_dataPath) {
-      // noop
+      if (options?.error) {
+        throw options.error;
+      }
     },
   };
 };
